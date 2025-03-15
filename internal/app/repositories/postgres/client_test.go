f302c755ecd5a60f36caf7f128686cd1dc6fe99a
tab <tukhfatullin@gmail.com>
2025-03-15 22:01:08 +0200

    chore(postgres): Add new tests for Postgres client

--- internal/app/repositories/postgres/client_test.go
+++ internal/app/repositories/postgres/client_test.go
@@ -3,8 +3,10 @@ package postgres
 import (
 	"testing"
 
+	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/stretchr/testify/assert"
 
+	"loki/internal/app/repositories/db"
 	"loki/internal/config"
 )
 
@@ -16,7 +18,6 @@ func Test_NewPostgresClient(t *testing.T) {
 	tests := []struct {
 		name string
 		args args
-		err  bool
 	}{
 		{
 			name: "Success",
@@ -25,7 +26,6 @@ func Test_NewPostgresClient(t *testing.T) {
 					DatabaseDSN: "postgres://localhost:5432",
 				},
 			},
-			err: false,
 		},
 	}
 
@@ -34,6 +34,70 @@ func Test_NewPostgresClient(t *testing.T) {
 			result, err := NewPostgresClient(tt.args.cfg)
 			assert.NoError(t, err)
 			assert.NotNil(t, result)
+		})
+	}
+}
+
+func Test_Postgres_Db(t *testing.T) {
+	type args struct {
+		cfg *config.Config
+	}
+
+	tests := []struct {
+		name string
+		args args
+	}{
+		{
+			name: "Success",
+			args: args{
+				cfg: &config.Config{
+					DatabaseDSN: "postgres://localhost:5432",
+				},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			client, err := NewPostgresClient(tt.args.cfg)
+			assert.NoError(t, err)
+
+			pool := client.Db()
+
+			assert.NotNil(t, pool)
+			assert.IsType(t, &pgxpool.Pool{}, pool)
+		})
+	}
+}
+
+func Test_Postgres_Queries(t *testing.T) {
+	type args struct {
+		cfg *config.Config
+	}
+
+	tests := []struct {
+		name string
+		args args
+	}{
+		{
+			name: "Success",
+			args: args{
+				cfg: &config.Config{
+					DatabaseDSN: "postgres://localhost:5432",
+				},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			client, err := NewPostgresClient(tt.args.cfg)
+			assert.NoError(t, err)
+
+			queries := client.Queries()
+
+			assert.NotNil(t, queries)
+			assert.IsType(t, &db.Queries{}, queries)
 		})
 	}
 }
