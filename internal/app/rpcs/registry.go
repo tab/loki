96a7604f511859d29af9cedbb120f4d20ff7da3a
tab <tukhfatullin@gmail.com>
2025-04-01 22:07:28 +0300

    feat(grpc) Add gRPC scope service
    Added scope service with CRUD operations for managing application scopes

--- internal/app/rpcs/registry.go
+++ internal/app/rpcs/registry.go
@@ -1,23 +1,27 @@
 package rpcs
 
 import (
-    "google.golang.org/grpc"
+	"google.golang.org/grpc"
 
-    proto "loki/internal/app/rpcs/proto/sso/v1"
+	proto "loki/internal/app/rpcs/proto/sso/v1"
 )
 
 type Registry struct {
-    permissions proto.PermissionServiceServer
+	permissions proto.PermissionServiceServer
+	scopes      proto.ScopeServiceServer
 }
 
 func NewRegistry(
-    permissions proto.PermissionServiceServer,
+	permissions proto.PermissionServiceServer,
+	scopes proto.ScopeServiceServer,
 ) *Registry {
-    return &Registry{
-        permissions: permissions,
-    }
+	return &Registry{
+		permissions: permissions,
+		scopes:      scopes,
+	}
 }
 
 func (r *Registry) RegisterAll(server *grpc.Server) {
-    proto.RegisterPermissionServiceServer(server, r.permissions)
+	proto.RegisterPermissionServiceServer(server, r.permissions)
+	proto.RegisterScopeServiceServer(server, r.scopes)
 }
